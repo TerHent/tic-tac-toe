89f08b2336a950d1101d918f0f7bb614a9e932a0
TerHent <timur10012@gmail.com>
2024-05-15 14:46:31 +0200

    add display

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ class Board(tk.Tk):
         super().__init__()
         self.title("Tic-Tac-Toe Game")
         self._cels = {}
+
+    def _create_board_display(self):
+        display_frame = tk.Frame(master=self)
+        display_frame.pack(fill=tk.X)
+        self.display = tk.Label(
+            master = display_frame,
+            text = "Ready?",
+            font = font.Font(size = 28, weight = "bold"),
+        )
+        self.display.pack()
