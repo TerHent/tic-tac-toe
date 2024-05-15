260347bf1c52ef1cbc1a53040dc9a0feaa22081a
TerHent <timur10012@gmail.com>
2024-05-15 15:02:44 +0200

    added board grid

--- main.py
+++ main.py
@@ -8,11 +8,36 @@ class Board(tk.Tk):
         self._cels = {}
 
     def _create_board_display(self):
-        display_frame = tk.Frame(master=self)
-        display_frame.pack(fill=tk.X)
+        display_frame = tk.Frame(master = self)
+        display_frame.pack(fill = tk.X)
         self.display = tk.Label(
             master = display_frame,
             text = "Ready?",
             font = font.Font(size = 28, weight = "bold"),
         )
         self.display.pack()
+
+    def _create_board_grid(self):
+        grid_frame = tk.Frame(master = self)
+        grid_frame.pack()
+        for row in range(3):
+            self.rowconfigure(row, weight = 1, minsize = 50)
+            self.columnconfigure(row, weight = 1, minsize = 50)
+            for col in range(3):
+                button = tk.Button(
+                    master = grid_frame,
+                    text = "",
+                    font = font.Font(size = 36, weight = "bold"),
+                    fg = "black",
+                    width = 3,
+                    height = 3,
+                    highlightbackground = "lightblue",
+                )
+                self._cels[button] = (row, col)
+                button.grid(
+                    row = row,
+                    column = col,
+                    padx = 5,
+                    pady = 5,
+                    sticky = "nsew"
+                )
